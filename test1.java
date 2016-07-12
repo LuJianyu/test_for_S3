d48924069f7bd2cbd571c763c974d3d5a68e791f
LuJianyu <ljiy369@gmail.com>
2016-07-12 17:00:04 +0800

    first commit for S3

---
+++ test1.java
@@ -0,0 +1,138 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.UUID;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+
+public class test1 {
+
+    public static void main(String[] args) throws IOException {
+
+       
+        AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+
+        AmazonS3 s3 = new AmazonS3Client(credentials);
+        Region ap_sea = Region.getRegion(Regions.AP_SOUTHEAST_1);
+        s3.setRegion(ap_sea);
+
+        String bucketName = "testssss";
+        String key = "MyObjectKey";
+
+        
+        try {
+            
+           
+            
+            
+            for (Bucket bucket : s3.listBuckets()) {
+                System.out.println(" - " + bucket.getName());
+            }
+            System.out.println();
+
+            
+            System.out.println("Uploading a new object to S3 from a file\n");
+            s3.putObject(new PutObjectRequest(bucketName, key, createSampleFile()));
+
+            
+            
+            System.out.println("Listing objects");
+            ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
+                    .withBucketName(bucketName)
+                    .withPrefix("My"));
+            for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+                System.out.println(" - " + objectSummary.getKey() + "  " +
+                                   "(size = " + objectSummary.getSize() + ")");
+            }
+            System.out.println();
+/*
+            System.out.println("Deleting an object\n");
+            s3.deleteObject(bucketName, key);
+*/
+            
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to Amazon S3, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with S3, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+
+  
+    private static File createSampleFile() throws IOException {
+    	String numberString = "";
+    	File file =new File("randomNumber.txt");
+		for(int i=0;i<10;i++){
+			double n = Math.random();
+			n *= 100;
+			int m = (int)n;
+			numberString = numberString + Integer.toString(m) +" "; 
+			System.out.println(m);	
+		}
+		try{
+		      String data = numberString;
+
+		      
+
+		      //if file doesnt exists, then create it
+		      if(!file.exists()){
+		       file.createNewFile();
+		      }
+
+		      //true = append file
+		      FileWriter fileWritter = new FileWriter(file.getName(),true);
+		             BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+		             bufferWritter.write(data);
+		             bufferWritter.close();
+
+		         System.out.println("Done");
+
+		     }catch(IOException e){
+		      e.printStackTrace();
+		     }
+        return file;
+    }
+
+   
+
+}
